3b83c74321a1775c0f497eef6baebf5640760c20
Björn Wilmsmann <bjoern@wilmsmann.de>
2013-07-09 17:37:10 +0200

    script customizations

--- casperjs/filter.js
+++ casperjs/filter.js
@@ -1,5 +1,29 @@
 var casper = require('casper').create();
 
-casper.setFilter('open.location', function(location) {
-    return /\?+/.test(location) ? location += "&foo=42" : location += "?foo=42";
+casper.on('http.status.200', function(resource) {
+    this.echo(resource.url + ' is OK', 'INFO');
 });
+
+casper.on("http.status.301", function(resource) {
+    this.echo(resource.url + " is permanently redirected", "PARAMETER");
+});
+
+casper.on("http.status.302", function(resource) {
+    this.echo(resource.url + " is temporarily redirected", "PARAMETER");
+});
+
+casper.on("http.status.404", function(resource) {
+    this.echo(resource.url + " is not found", "COMMENT");
+});
+
+casper.on("http.status.500", function(resource) {
+    this.echo(resource.url + " is in error", "ERROR");
+});
+
+casper.setFilter('open.location', function(location) {
+    return /\?+/.test(location) ? location += '&foo=42' : location += '?foo=42';
+});
+
+casper.start();
+casper.thenOpen('http://google.com/');
+casper.run();

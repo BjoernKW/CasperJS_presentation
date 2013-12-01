d2c3a6b224f0f6eccfa5767949b996e256d69dd6
Björn Wilmsmann <bjoern@wilmsmann.de>
2013-12-01 20:17:21 +0100

    Test updates

--- run.js
+++
@@ -1,5 +0,0 @@
-casper.run(function() {
-    this.test.done(5);
-    this.test.renderResults(true, 0, 'results/test_results.xml');
-    this.test.viewport(1600, 1200);
-});
--- set_up.js
+++
@@ -1,3 +0,0 @@
-var start_url = 'http://www.google.de';
-
-casper.test.done();
